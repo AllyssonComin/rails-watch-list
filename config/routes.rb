bf4b2dbc813215d16ca5399b5a14166d3732188f
allyssoncomin <allysson_luan@hotmail.com>
2023-11-07 14:24:01 -0300

    basics + home page incomplet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
+# list routes
+  # Pages routes
+  root "pages#home"
+  get "about", to: "pages#about"
+
+  # List nested bookmarks routes
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  # Bookmarks routes
+  resources :bookmarks, only: [:destroy]
 end
